ec065434eae00cbb0e673b9fbd47495a12c8eb52
Eugene Tolmachev <eugene.tolmachev@prolucid.ca>
2015-09-15 13:38:14 -0400

    sample

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        class T {public int x {get;set;}}
+
+        static void Main(string[] args)
+        {
+            for (var ii = 0; ii < 4; ii++)
+            {
+				var sw = Stopwatch.StartNew();
+                Enumerable.Range(0,10000)
+                    .Select(i => (Expression<Func<T,bool>>)(t => t.x == i ))
+                    .ToList();
+				Console.WriteLine("Ellapsed: {0}", sw.Elapsed);
+            }
+        }
+    }
+}
---
+++ FSharpTest/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.Linq.Expressions
+open System.Diagnostics
+
+type Quote<'T> = 
+    static member X(exp:Expression<Func<'T,'a>>) = exp
+
+type T = { x:int }
+
+[<EntryPoint>]
+let main argv = 
+    for _ in [1..4] do
+        let sw = Stopwatch.StartNew()
+        [for i in [1..10000] -> Quote.X(fun x -> x.x = i)] |> ignore
+        sw.Stop()
+        printfn "Elapsed: %A" sw.Elapsed
+    0 // return an integer exit code
+
